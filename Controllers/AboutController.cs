02aaa1207121138f907159388fa163c33b0a56db
Anisha <anshusggl02@gmail.com>
2022-06-20 21:21:13 +0545

    added frontend

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult services()
+        {
+            return View();
+        }
+
+        public ViewResult login()
+        {
+            return View();
+        }
+
+
+        public ViewResult Signin()
+        {
+            return View();
+        }
+
+        public ViewResult Opinions()
+        {
+            return View();
+        }
+
+        public ViewResult StudentResult()
+        {
+            return View();
+        }
+
+        public ViewResult NextExam()
+        {
+            return View();
+        }
+
+    }
+}
